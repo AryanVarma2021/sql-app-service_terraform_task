99ec5f627aa056daa9d13f2563719f6f327a9dea
aryan_pramodkumarvarma <aryan_pramodkumarvarma@epam.com>
2026-04-23 05:56:07 +0000

    sql , app service terraform task done

--- task06/modules/sql/outputs.tf
+++ task06/modules/sql/outputs.tf
@@ -4,7 +4,7 @@ output "sql_server_fqdn" {
 
 }
 
-output "sql_connection_string " {
+output "sql_connection_string" {
   value     = "Server=tcp:${azurerm_mssql_server.sql_server.fully_qualified_domain_name};Database=${azurerm_mssql_database.sql_db.name};User ID=${var.administrator_login};Password=${random_password.sql_password_generator.result};"
   sensitive = true
 }
